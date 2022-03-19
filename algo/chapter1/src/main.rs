ab72108c7fb6cd4793eefed35ebe8b99ba3f3193
Anthony Tortorello <tortorello.anthony@gmail.com>
2022-03-19 11:26:35 -0400

    feat: exercise 1.1.3, faster euclid algorithm

--- algo/chapter1/src/main.rs
+++ algo/chapter1/src/main.rs
@@ -1,5 +1,12 @@
 use std::fmt;
 
+// Definiteness: make sure that m and n are postitive numbers
+// by using signed integers
+struct EuclidInput {
+    m: i32,
+    n: i32,
+}
+
 #[derive(Debug, PartialEq)]
 struct Answer {
     gcd: i32,
@@ -12,16 +19,17 @@ impl fmt::Display for Answer {
     }
 }
 
+// Euclids algo (E)
 // we want m and n to be owned so we do not
 // take over ownership of the inputted values
-//
-// Definiteness: make sure that m and n are postitive numbers
-// by using signed integers
-fn euclids(mut m: i32, mut n: i32) -> Answer {
+fn algo_e(e_input: &EuclidInput) -> Answer {
     // given 2 positive integers m and n
     // find their greatest common divisor
     // the largest possible integer that evenly
     // divides both m and n
+
+    let mut m = e_input.m;
+    let mut n = e_input.n;
 
     // E0
     if m < n {
@@ -47,22 +55,75 @@ fn euclids(mut m: i32, mut n: i32) -> Answer {
     }
 }
 
+fn algo_f(e_input: &EuclidInput) -> Answer {
+    let mut m = e_input.m;
+    let mut n = e_input.n;
+    let mut i = 1;
+
+    loop {
+        // F1 divide m by n and let m be the remainder
+        m = m % n;
+
+        match m {
+            // F2: if m=0 then answer is n
+            0 => break Answer { gcd: n, iter: i },
+            _ => {
+                // F3: divide n by m and let n be the remainder
+                n = n % m;
+                match n {
+                    // F4: if n=0 then answer is m
+                    0 => break Answer { gcd: m, iter: i  },
+                    _ => {
+                        i = i + 1;
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Chapter 1");
 
+    println!("Euclid's algorithm");
     // TODO - read from command line user input
-    let m = 119;
-    let n = 544;
-    let euc = euclids(m.clone(), n.clone());
-    println!("{}", &euc);
+    let m = 6099;
+    let n = 2166;
+    let e_input = EuclidInput {
+        m: m.clone(),
+        n: n.clone(),
+    };
+    let euc_e = algo_e(&e_input);
+    println!("{}", &euc_e);
+
+    println!("Section 1.1 Exercises");
+
+    // Number 3
+    println!("1.1.3");
+    // Change algo E so that all trivial replacement operations
+    // such as m <- n are avoided, call this algorithm f
+    // Faster!!!
+    let euc_f = algo_f(&e_input);
+    println!("{}", &euc_f);
 }
 
 #[test]
-fn euclid_slow() {
+fn euclid_e() {
     let right_answer = Answer { gcd: 17, iter: 5 };
-    let answer = euclids(119, 544);
+    // TODO: macro
+    let answer = algo_e(&EuclidInput { m: 119, n: 544 });
+
     assert_eq!(right_answer, answer);
 
     // TODO
     // m = qn + r
+}
+
+#[test]
+fn euclid_f() {
+    let right_answer = Answer { gcd: 17, iter: 3 };
+    let answer = algo_f(&EuclidInput { m: 119, n: 544 });
+
+    assert_eq!(right_answer, answer);
 }
